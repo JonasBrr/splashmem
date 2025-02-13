2711bc641f53beac64bba5d5b3bcfa01b19fb520
JonasBrr <jonas.bourreau@gmail.com>
2025-02-13 15:12:17 +0100

    Init Commit

---
+++ src/render.c
@@ -0,0 +1,91 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "actions.h"
+#include "param.h"
+#include "splash.h"
+#include "world.h"
+
+/* ------------------------------------------------------------------------- */
+/*                                                                           */
+/* ------------------------------------------------------------------------- */
+void render_map()
+{
+    int      i = 0;
+    SDL_Rect rect;
+
+    rect.x = 0;
+    rect.y = 0;
+    rect.w = SQUARE_SIZE;
+    rect.h = SQUARE_SIZE;
+
+    screenSurface = SDL_GetWindowSurface(window);
+
+    for (i = 0; i < SPOT_NUM; i++)
+    {
+        rect.x = (i % MAP_SIZE) * SQUARE_SIZE;
+        rect.y = (i / MAP_SIZE) * SQUARE_SIZE;
+        SDL_FillRect(screenSurface, &rect, colors[mapmem[i]]);
+    }
+    SDL_UpdateWindowSurface(window);
+}
+
+/* ------------------------------------------------------------------------- */
+/*                                                                           */
+/* ------------------------------------------------------------------------- */
+void render_player()
+{
+    int i         = 0;
+    screenSurface = SDL_GetWindowSurface(window);
+    SDL_Rect rect;
+
+    rect.x = 0;
+    rect.y = 0;
+    rect.w = SQUARE_SIZE;
+    rect.h = SQUARE_SIZE;
+
+    for (i = 0; i < MAX_PLAYERS; i++)
+    {
+        rect.x = players[i]->x * SQUARE_SIZE;
+        rect.y = players[i]->y * SQUARE_SIZE;
+        SDL_FillRect(screenSurface, &rect, colors[i] - 0x00202020);
+    }
+    SDL_UpdateWindowSurface(window);
+}
+
+/* ------------------------------------------------------------------------- */
+/*                                                                           */
+/* ------------------------------------------------------------------------- */
+void main_loop()
+{
+    // int is_game_finish = 0;   vous pouvez l'utilisez
+
+    while (!quitting)
+    {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                quitting = 1;
+            }
+        }
+
+        players[1]->x += 1;
+        if (players[1]->x >= MAP_SIZE)
+        {
+            players[1]->x = 0;
+        }
+
+        world_paint_spot(players[0]->x, players[0]->y, 1); // 1 for player1 <-> idx 0
+        printf("x = %d\n", players[0]->x);
+
+        render_map();
+
+        SDL_Delay(100);
+    }
+}
